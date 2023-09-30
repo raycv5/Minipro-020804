da7cb67c7992b9ba80990761dd15bc977e8ca69a
raycv5 <rayhanaw612@gmail.com>
2023-09-30 13:31:34 +0700

    Install tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/pages/**/*.{jsx,js}", "./src/components/**/*.{jsx,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
